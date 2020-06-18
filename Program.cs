a0128de5a228976541a4cd5902809de07d357be0
EmilianaFunes <63813666+EmilianaFunes@users.noreply.github.com>
2020-06-18 10:13:21 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,10 +5,47 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace Trabajo_Práctico_N_2
 {
-    class Program
+    class GestorCotización
     {
+        public static List<cotizacion> Cotizaciones = new List<cotizacion>();
         static void Main(string[] args)
         {
+            //Registro Cliente
+            Console.WriteLine("Ingrese el nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("\nIngrese la empresa: ");
+            string empresa = Console.ReadLine();
+            Console.WriteLine("\nIngrese direccion de obra: ");
+            string direccionobra = Console.ReadLine();
+            Console.WriteLine("\nIngrese el mail: ");
+            string mail = Console.ReadLine();
+            Console.WriteLine("\nIngrese el telefono: ");
+            string telefono = Console.ReadLine();
+
+            cliente cliente = new cliente(nombre, empresa, direccionobra, mail, telefono);
+            RegistroCliente.Clientes.Add(cliente);
+
+            //Elegir Metros Cuadrados y material
+            Console.WriteLine("\n \nIngrese los metros cuadrados a cubrir: ");
+            double metrosCuadrados = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n \n Seleccione el material a utilizar: ");
+            RegistroMaterial.MostrarMaterial();
+
+            Console.WriteLine("\n Su opción: ");
+            var opcion = Console.ReadLine();
+            var material = RegistroMaterial.Materiales[int.Parse(opcion) - 1];
+
+            //Elegir Espesor
+
+            Console.WriteLine("\n Seleccione el espesor que desea colocar: ");
+            RegistroEspesor.MostrarEspesor();
+
+            Console.WriteLine("\n Su opcion: ");
+            var OpcionEspesor = Console.ReadLine();
+            var espesor = RegistroEspesor.Espesores[int.Parse(OpcionEspesor) - 1];
+
+
 
         }
     }
@@ -58,24 +95,36 @@ namespace Trabajo_Práctico_N_2
             MetroCuadrado = metroCuadrado;
             RendimientoPorBolsa = rendimientoporBolsa;
         }
-
-        class RegistroEspesor
-        {
-            public static List<espesor> Espesores = new List<espesor>();
-
-            static RegistroEspesor()
-            {
-                Espesores.Add(new espesor("Aplicado en Pared 50 mm", 53.60, 2.25));
-                Espesores.Add(new espesor("Aplicado en Pared 70 mm", 87.00, 3.15));
-                Espesores.Add(new espesor("Aplicado en Pared 100 mm", 117.49, 4.5));
-                Espesores.Add(new espesor("Aplicado en Pared 120 mm", 128.48, 5.4));
-                Espesores.Add(new espesor("Aplicado en Pared 160 mm", 143.405, 7.2));
-                Espesores.Add(new espesor("Aplicado en Pared 200 mm", 180.79, 9.00));
-            }
-        }
-
     }
 
+    class RegistroEspesor
+    {
+        public static List<espesor> Espesores = new List<espesor>();
+
+        static RegistroEspesor()
+        {
+            Espesores.Add(new espesor("Aplicado en Pared 50 mm", 53.60, 2.25));
+            Espesores.Add(new espesor("Aplicado en Pared 70 mm", 87.00, 3.15));
+            Espesores.Add(new espesor("Aplicado en Pared 100 mm", 117.49, 4.5));
+            Espesores.Add(new espesor("Aplicado en Pared 120 mm", 128.48, 5.4));
+            Espesores.Add(new espesor("Aplicado en Pared 160 mm", 143.405, 7.2));
+            Espesores.Add(new espesor("Aplicado en Pared 200 mm", 180.79, 9.00));
+        }
+
+        public static void MostrarEspesor()
+        {
+            
+            Console.WriteLine("LISTADO ESPESORES: ");
+            int pos = 1;
+
+            foreach (var espesor in RegistroEspesor.Espesores)
+            {
+                Console.WriteLine();
+                Console.WriteLine(pos + ")" + espesor.descripcion + "--->" + "Precio por metro cuadrado: $" + espesor.MetroCuadrado + "--" + "Rendimiento por bolsa:" + espesor.RendimientoPorBolsa);
+                pos = pos + 1;
+            }
+        }
+    }
     public class cliente
     {
        public string Nombre { get; set; }
@@ -94,8 +143,25 @@ namespace Trabajo_Práctico_N_2
         }
     }
     
-        
+    class RegistroCliente
+    {
+        public static List<cliente> Clientes = new List<cliente>();
 
+        static void MostrarClientes()
+        {
+
+            Console.WriteLine("Listado clientes: ");
+            int pos = 1;
+
+            foreach (var cliente in RegistroCliente.Clientes)
+            {
+                Console.WriteLine();
+                Console.WriteLine("$ pos + Nombre: {cliente.Nombre}, de la empresa: {cliente.Empresa}");
+                pos = pos + 1;
+            }
+        }
+    }
+        
     public class material
     {
        public string descripcion { get; set; }
@@ -118,48 +184,19 @@ namespace Trabajo_Práctico_N_2
             Materiales.Add(new material("Aislante nn 123", 3500));
             Materiales.Add(new material("Aislante nn 566", 550));
         }
-    }
 
         public static void MostrarMaterial()
         {
-            Console.WriteLine("Cotizador: ");
-            Console.WriteLine("Listado materiales: ");
+            
+            Console.WriteLine("LISTADO MATERIALES: ");
             int pos = 1;
 
             foreach (var material in RegistroMaterial.Materiales)
             {
                 Console.WriteLine();
-                Console.WriteLine("$pos + { material.descripcion} $Precio por bolsa: {material.PrecioBolsa}");
-            pos = pos + 1;
+                Console.WriteLine(pos + ")"  + material.descripcion + "-----> Precio por bolsa: $"  + material.precioBolsa);
+                pos = pos + 1;
             }
         }
-
-        public static void MostrarEspesor()
-          {
-        Console.WriteLine("Cotizador: ");
-        Console.WriteLine("Listado espesores: ");
-        int pos = 1;
-
-        foreach (var espesor in RegistroEspesor.Espesores)
-        {
-            Console.WriteLine();
-            Console.WriteLine("$ pos + { espesor.descripcion} $Precio por metro cuadrado: {espesor.MetroCuadrado}, Rendimiento por bolsa: {espesor.RendimientoPorBolsa");
-            pos = pos + 1;
-        }
     }
-
-    public static void MostrarClientes ()
-    {
-        
-        Console.WriteLine("Listado clientes: ");
-        int pos = 1;
-
-        foreach (var cliente in RegistroCliente.clientes)
-        {
-            Console.WriteLine();
-            Console.WriteLine("$ pos + Nombre: {cliente.Nombre}, de la empresa: {cliente.Empresa}");
-            pos = pos + 1;
-        }
-    }
-
 }
